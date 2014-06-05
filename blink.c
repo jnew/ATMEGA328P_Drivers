b6c21582b46301989ce0f36da813ae5512d441b8
John New <john.new@kollmorgen.com>
2014-06-05 18:39:52 -0400

    Initial commit

---
+++ blink.c
@@ -0,0 +1,81 @@
+#include "usart.h"
+
+volatile uint32_t count;
+
+void TIMER0_OVF_vect(void) __attribute__ ((signal));
+
+void TIMER0_OVF_vect(void)
+{
+	count++;
+}
+
+uint8_t string_compare(uint8_t *arg1, uint8_t *arg2, uint8_t length)
+{
+	int i;
+	for(i = 0; i < length; i++)
+	{
+		if(arg1[i] != arg2[i])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main(void)
+{	
+	uint8_t led_state = 0;
+	uint8_t command[50];
+	uint8_t command_length;
+	
+	//set DDRB so Pin B5 is output
+	*(uint8_t *)(0x24) |= (1<<5);
+	
+	//set up timer
+	*(uint8_t *)(0x44) = (0x00);
+	*(uint8_t *)(0x45) |= (0x01);
+	
+	//enable T0 ovf interrupt
+	//*(uint8_t *)(0x6E) |= (0x01);
+	
+	//global interrupt enable
+	asm("sei"::);
+	
+	usart0_config();
+	usart0_enable_trans();
+	usart0_enable_recv();
+	
+	usart0_send_string("\r\n# ", 4);
+	
+	while(1) {
+		command_length = usart0_recv_line(command);
+		if(command_length != 0) {
+			usart0_send_byte('\n');
+			if(string_compare(command, "BLINK_OFF", 9)) {
+				usart0_send_string("Stopping timer0. \r\n", 19);
+				// disable timer0 int
+				*(uint8_t *)(0x6E) &= ~(0x01);
+			} else if(string_compare(command, "BLINK_ON", 8)) {
+				usart0_send_string("Starting timer0. \r\n", 19);
+				// enable timer0 int
+				*(uint8_t *)(0x6E) |= (0x01);
+			} else if(string_compare(command, "THE_TRUTH", 9)) {
+				usart0_send_string("John New is the best developer in the world. \r\n", 47);
+			}
+			usart0_send_string("# ", 2);
+		} else if(count >= 62500) {
+			if(led_state == 0) {	//set led on
+				*(uint8_t *)(0x25) |= (1<<5);
+				led_state = 1;
+			}
+			else {					//set led off
+				*(uint8_t *)(0x25) &= ~(1<<5);
+				led_state = 0;
+			}
+			count = 0;
+		}
+	}
+	
+	return 0;
+}
+
---
+++ usart.h
@@ -0,0 +1,59 @@
+/* This is a simple usart driver for ATMEGA328P. 
+	Author: John New
+	Last Revision: 6-5-14
+*/
+
+#ifndef USART_H
+#define USART_H
+
+#include <stdint.h>
+#include <avr/io.h>
+
+// This struct allows interrupt driver multi-frame transfers.
+typedef struct
+{
+	volatile uint8_t send_buffer[256];
+	volatile uint8_t recv_buffer[256];
+	volatile uint8_t send_buffer_head;
+	volatile uint8_t send_buffer_tail;
+	volatile uint8_t recv_buffer_head;
+	volatile uint8_t recv_buffer_tail;
+} usart;
+
+usart usart_driver;
+
+// Configure as 19200 BAUD, 8 data bits, 1 stop bit.
+void usart0_config();
+
+// Enable TX.
+void usart0_enable_trans();
+
+// Disable TX.
+void usart0_disable_trans();
+
+// Enable RX.
+void usart0_enable_recv();
+
+// Disable RX.
+void usart0_disable_recv();
+
+// Send one byte.
+void usart0_send_byte(uint8_t data);
+
+// Send a string of variable length.
+void usart0_send_string(uint8_t *data, uint8_t string_length);
+
+// Pull one byte out of the recieve buffer.
+uint8_t usart0_recv_byte(uint8_t *data);
+
+// Transmit complete interrupt handler.
+void USART_TX_vect(void) __attribute__ ((signal));
+
+// Receive complete interrupt handler.
+void USART_RX_vect(void) __attribute__ ((signal));
+
+// USART ready interrupt handler.
+void USART_UDRE_vect(void) __attribute__ ((signal));
+
+
+#endif
